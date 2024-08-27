91074a0381439c440e93575c616c89562ece8778
Gospel Jonathan <sirelite11@gmail.com>
2024-08-27 17:09:16 +0100

    fixed login logo

--- src/components/layout/AuthLayout/AuthLayout.tsx
+++ src/components/layout/AuthLayout/AuthLayout.tsx
@@ -2,19 +2,23 @@ import { Box } from "@mui/material";
 
 import { Link, Outlet } from "react-router-dom";
 import logo from "@/assets/images/Logo.png";
+import Footer from "../GeneralLayout/Footer";
 
 const AuthLayout = () => {
   return (
-    <Box className="bg-[#e7e7e7] h-screen flex justify-center items-center ">
-      <Box className="px-8 absolute top-5 left-10 sm:py-2 py-8">
-        <Link to="/">
-          <img src={logo} alt="Logo of BST" className="sm:w-[12%] w-[25%]" />
-        </Link>
+    <>
+      <Box className="bg-[#e7e7e7] h-screen flex justify-center items-center ">
+        <Box className="sm:px-8 px-2 absolute top-5 sm:left-10 left-0 sm:py-2 py-8">
+          <Link to="/">
+            <img src={logo} alt="Logo of BST" className="sm:w-[12%] w-[55%]" />
+          </Link>
+        </Box>
+        <Box className="w-full ">
+          <Outlet />
+        </Box>
       </Box>
-      <Box className="w-full ">
-        <Outlet />
-      </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
--- src/services/api/axiosClient.ts
+++ src/services/api/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3007/bst/v1",
+  // baseURL: "http://localhost:3007/bst/v1",
+  baseURL: "https://bst-refer-backend.onrender.com/bst/v1",
   headers: {
     "Content-Type": "application/json",
   },
